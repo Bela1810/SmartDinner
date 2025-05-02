f43bfa2971d356af10dcca923d9c93e8664d4864
Bela <belacaste3@gmail.com>
2025-05-02 15:16:35 -0500

    borrando carpetas que no se usan

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:smartdinner/ui/screens/auth/auth_screen.dart';
-import 'package:smartdinner/ui/screens/table_screen/table_screen.dart';
 import 'package:smartdinner/ui/screens/admin_screens/admin_home.dart';
-import 'package:smartdinner/ui/screens/admin_screens/menu_list_screen.dart'; 
-import 'package:smartdinner/ui/screens/admin_screens/dish_list_screen.dart';
-import 'firebase_options.dart';
 
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -24,41 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
-      home: const AuthWrapper(),
-      routes: {
-        '/admin': (context) => const AdminHome(), 
-        '/menus': (context) => const MenuListScreen(),
-        '/dis': (context) => const DishListScreen(),
-      },
-    );
-  }
-}
-
-class AuthWrapper extends StatelessWidget {
-  const AuthWrapper({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        if (snapshot.hasData) {
-          return const TableScreen();
-        } else {
-          return const AuthScreen();
-        }
-      },
+      home: AdminHome(),
     );
   }
 }
